76edaa0eb9fde79ae58deb05f4da83bed4e65764
jackievalori <63308026+jackievalori@users.noreply.github.com>
2022-12-21 18:29:41 -0500

    setup for github pages

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 // import { vitePreprocess } from '@sveltejs/kit/vite';
 import preprocess from 'svelte-preprocess';
 
+const githubPagesUrl = 'https://cardinal-designs.github.io/malla-quiz/';
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -10,11 +12,16 @@ const config = {
 
 	kit: {
 		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
+			pages: 'docs',
+			assets: 'docs',
 			fallback: null,
 			precompress: false,
-			strict: true
+			strict: true,
+			// hydrate the <div id="svelte"> element in src/app.html
+			target: '#svelte',
+			paths: {
+				base: process.env.NODE_ENV === 'production' ? githubPagesUrl : ''
+			}
 		})
 	}
 };
