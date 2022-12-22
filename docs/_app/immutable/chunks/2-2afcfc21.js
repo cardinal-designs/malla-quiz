81bff418713b27d6b4c7c0e9b9dc513cbda81b06
jackievalori <63308026+jackievalori@users.noreply.github.com>
2022-12-21 19:18:47 -0500

    updated hover styles

--- docs/_app/immutable/chunks/2-2afcfc21.js
+++
@@ -1,1 +0,0 @@
-import{default as t}from"../components/pages/_page.svelte-0c3abdbb.js";export{t as component};
---
+++ docs/_app/immutable/chunks/2-ea343a4e.js
@@ -0,0 +1,1 @@
+import{default as t}from"../components/pages/_page.svelte-082fc410.js";export{t as component};
